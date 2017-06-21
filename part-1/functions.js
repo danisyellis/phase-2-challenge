deebd6bc746a1c6485db8da2a57e2d2e9775d39e
Danielle Gellis <danisyellis@gmail.com>
2017-06-21 10:37:57 -0700

    adds tests and working function for 'show a weekday'

--- part-1/functions.js
+++ part-1/functions.js
@@ -1,8 +1,12 @@
-module.exports = function() {
-  return 'hello1';
+
+var weekDay = function(date) {
+  var day = date.toString().slice(0,3);
+  if(Object.prototype.toString.call(date) !== '[object Date]') {
+    return false;
+  }
+  return day
 }
 
-
-// var weekDay = function(date) {
-//
-// }
+module.exports =  {
+  weekDay
+}
--- part-1/test/tests.js
+++ part-1/test/tests.js
@@ -2,7 +2,12 @@ const assert = require('chai').assert;
 const functions = require('../functions.js');
 
 describe('myFunctions', function() {
-  it('function should return hello', function() {
-    assert.equal(functions(), 'hello');
+  it('weekDay should return Mon', function() {
+    var result = functions.weekDay(new Date(2017, 5, 19))
+    assert.equal(result, 'Mon');
   });
+  it('weekDay should return false if it is not given a date', function() {
+    var result = functions.weekDay("oh hai!");
+    assert.equal(result, false)
+  })
 })
